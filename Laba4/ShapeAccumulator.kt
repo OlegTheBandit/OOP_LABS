2d839c2be3c2a8102ae970cd853d52343dbb2139
Tabanakov Oleg <45435763+TabanakovOleg@users.noreply.github.com>
2020-11-11 13:08:30 +0300

    4laba

---
+++ Laba4/ShapeAccumulator.kt
@@ -0,0 +1,53 @@
+class ShapeAccumulator {
+    var shapes = arrayListOf<Shape>()
+
+    fun<T : Shape>  add(figure: T){
+        shapes.add(figure)
+    }
+
+    fun<T : Shape>  addAll(collection: Collection<T>){
+        collection.forEach(){shapes.add(it)}
+    }
+
+    fun getMaxAreaShape() : Shape {
+        if(shapes.isEmpty()){
+            throw NoSuchElementException("Our class is empty")
+        }
+        return shapes.maxBy(Shape::calcArea)!!
+    }
+
+    fun getMinAreaShape() : Shape {
+        if(shapes.isEmpty()){
+            throw NoSuchElementException("Our class is empty")
+        }
+        return shapes.minBy(Shape::calcArea)!!
+    }
+
+    fun getMaxPerimeterShape() : Shape {
+        if(shapes.isEmpty()){
+            throw NoSuchElementException("Our class is empty")
+        }
+        return shapes.maxBy(Shape::calcPerimeter)!!
+    }
+
+    fun getMinPerimeterShape() : Shape {
+        if(shapes.isEmpty()){
+            throw NoSuchElementException("Our class is empty")
+        }
+        return shapes.minBy(Shape::calcPerimeter)!!
+    }
+
+    fun getTotalArea() : Double {
+        if(shapes.isEmpty()){
+            throw NoSuchElementException("Our class is empty")
+        }
+        return shapes.sumByDouble { it.calcArea() }
+    }
+
+    fun getTotalPerimeter() : Double {
+        if(shapes.isEmpty()){
+            throw NoSuchElementException("Our class is empty")
+        }
+        return shapes.sumByDouble { it.calcPerimeter() }
+    }
+}
